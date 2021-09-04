a6ac18554677850ab66db8339d4b3d8a88402497
Dark Lord <larsenm2020@gmail.com>
2021-09-04 17:54:34 +0530

    heroku host test3

--- djangochat/djangochat/wsgi.py
+++ djangochat/djangochat/wsgi.py
@@ -8,8 +8,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+# from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dealloc.settings')
 
 application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
